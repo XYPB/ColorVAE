f497f047b19deefb7d129ae1aa0fdf2a3da9d807
XYPB <duyx2000@gmail.com>
2021-04-01 12:28:56 +0800

    minor fix shuffle

--- main.py
+++ main.py
@@ -8,80 +8,81 @@ from schedular import LinearWarmupScheduler
 from data import get_data_loaders, reconstruct
 from model import get_model
 
-device = 'cuda'
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-############
-##  Data  ##
-############
+if __name__=='__main__':
+    ############
+    ##  Data  ##
+    ############
 
-torch.manual_seed(0)
-tr_loader, va_loader = get_data_loaders(64)
+    torch.manual_seed(0)
+    tr_loader, va_loader = get_data_loaders(64)
 
-#############
-##  Model  ##
-#############
+    #############
+    ##  Model  ##
+    #############
 
-model.decoder.net.backbone.require_grad = False
-model.decoder.net.backbone.eval()
-model = get_model().to(device)
-optim = torch.optim.SGD(filter(lambda p: p.require_grad, model.parameters()), lr=1e-2, momentum=0.9, weight_decay=1e-4)
-sched = LinearWarmupScheduler(optim, 1000)
+    model = get_model().to(device)
+    model.decoder.net.backbone.requires_grad = False
+    model.decoder.net.backbone.eval()
+    optim = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-2, momentum=0.9, weight_decay=1e-4)
+    sched = LinearWarmupScheduler(optim, 1000)
 
-###############
-##  Logging  ##
-###############
+    ###############
+    ##  Logging  ##
+    ###############
 
-from tensorboardX import SummaryWriter
-writer = SummaryWriter(flush_secs=30)
-gIter = 0
+    from tensorboardX import SummaryWriter
+    writer = SummaryWriter(flush_secs=30)
+    gIter = 0
 
-X_test, y_test = next(iter(va_loader))
-X_test = X_test.to('cuda')
-y_test = y_test.to('cuda')
+    X_test, y_test = next(iter(va_loader))
+    X_test = X_test.to('cuda')
+    y_test = y_test.to('cuda')
 
 
-for epoch in range(16):
-    cum_loss = 0.0
-    pbar = tqdm(tr_loader)
-    model.encoder.train()
-    for i, (l, ab) in enumerate(pbar):
-        l = l.to(device)
-        ab = ab.to(device)
-        loss = -model.log_prob(ab, l).mean() / (64 * 64 * 2)
-
-        optim.zero_grad()
-        loss.backward()
-        optim.step()
-        sched.step()
-        cum_loss += loss.item()
-        pbar.set_description_str(f"Epoch {epoch}, nll {cum_loss / (i+1):.4f}")
-        writer.add_scalar("Train/nll", loss, gIter)
-        gIter += 1
-
-    model.encoder.eval()
-    with torch.no_grad():
+    for epoch in range(16):
         cum_loss = 0.0
-        pbar = tqdm(va_loader)
+        pbar = tqdm(tr_loader)
+        model.encoder.train()
         for i, (l, ab) in enumerate(pbar):
             l = l.to(device)
             ab = ab.to(device)
             loss = -model.log_prob(ab, l).mean() / (64 * 64 * 2)
+
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            sched.step()
             cum_loss += loss.item()
-            pbar.set_description_str(f"Test nll {cum_loss / (i+1):.4f}")
-    writer.add_scalar("Val/nll", cum_loss / len(va_loader), gIter)
+            pbar.set_description_str(f"Epoch {epoch}, nll {cum_loss / (i+1):.4f}")
+            writer.add_scalar("Train/nll", loss, gIter)
+            gIter += 1
 
-    with torch.no_grad():
-        print(X_test.shape)
-        print(model.sample(X_test).shape)
-        lab = torch.cat([X_test, model.sample(X_test)], 1)
-        img = reconstruct(lab)
-        writer.add_images("result", img.transpose(0, 3, 1, 2), gIter)
+        model.encoder.eval()
+        with torch.no_grad():
+            cum_loss = 0.0
+            pbar = tqdm(va_loader)
+            for i, (l, ab) in enumerate(pbar):
+                l = l.to(device)
+                ab = ab.to(device)
+                loss = -model.log_prob(ab, l).mean() / (64 * 64 * 2)
+                cum_loss += loss.item()
+                pbar.set_description_str(f"Test nll {cum_loss / (i+1):.4f}")
+        writer.add_scalar("Val/nll", cum_loss / len(va_loader), gIter)
 
-        l = lab[:1, 0].repeat(64, 1, 1, 1)
-        z = torch.meshgrid(torch.linspace(-2, 2, 8), torch.linspace(-2, 2, 8))
-        z = torch.stack(z, -1).reshape(64, 2, 1, 1).to(device)
-        lab = torch.cat([l, model.transform(z, l)], 1)
-        img = reconstruct(lab)
-        writer.add_images("sample", img.transpose(0, 3, 1, 2), gIter)
+        with torch.no_grad():
+            print(X_test.shape)
+            print(model.sample(X_test).shape)
+            lab = torch.cat([X_test, model.sample(X_test)], 1)
+            img = reconstruct(lab)
+            writer.add_images("result", img.transpose(0, 3, 1, 2), gIter)
 
-        torch.save(model.state_dict(), "model.pt")
+            l = lab[:1, 0].repeat(64, 1, 1, 1)
+            z = torch.meshgrid(torch.linspace(-2, 2, 8), torch.linspace(-2, 2, 8))
+            z = torch.stack(z, -1).reshape(64, 2, 1, 1).to(device)
+            lab = torch.cat([l, model.transform(z, l)], 1)
+            img = reconstruct(lab)
+            writer.add_images("sample", img.transpose(0, 3, 1, 2), gIter)
+
+            torch.save(model.state_dict(), "model.pt")
