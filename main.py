6fd19529543a1698b66068b2fd1d834e4b0954be
zyayoung <1328410180@qq.com>
2021-04-01 14:22:43 +0800

    fix

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ parser.add_argument('--using_vae', action='store_true')
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+os.makedirs(args.param_path, exist_ok=True)
+
 if __name__=='__main__':
     ############
     ##  Data  ##
