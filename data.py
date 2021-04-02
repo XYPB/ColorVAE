ffa5d51dc13beecaaa5c12d4523661ee0f503a46
zyayoung <1328410180@qq.com>
2021-04-03 01:33:54 +0800

    rbk

--- data.py
+++ data.py
@@ -8,7 +8,6 @@ import zipfile
 import os
 import io
 from PIL import Image
-import cv2
 
 
 mean = np.array([48., 2.5, 9.2], dtype=np.float32)
@@ -32,7 +31,7 @@ class ColorTinyImageNet(torchvision.datasets.VisionDataset):
             self.z_worker = zipfile.ZipFile(self.root)
         img = self.z_worker.read(path)
         img = Image.open(io.BytesIO(img)).convert('RGB')
-        lab = (rgb2lab(img) - mean) / std
+        lab = (np.float32(color.rgb2lab(img)) - mean) / std
         return lab[None, ..., 0], lab[..., 1:].transpose(2, 0, 1)
 
     def __len__(self) -> int:
@@ -50,7 +49,7 @@ class COCO(torchvision.datasets.VisionDataset):
         img = Image.open(os.path.join(self.root, path)).convert('RGB')
         if self.transform is not None:
             img = self.transform(img)
-        lab = (rgb2lab(img) - mean) / std
+        lab = (np.float32(color.rgb2lab(img)) - mean) / std
         return lab[None, ..., 0], lab[..., 1:].transpose(2, 0, 1)
 
     def __len__(self) -> int:
@@ -58,11 +57,10 @@ class COCO(torchvision.datasets.VisionDataset):
 
 mean = np.array([48., 2.5, 9.2], dtype=np.float32)
 std = np.array([27., 13., 18.], dtype=np.float32)
-
 class ColorImageNet(torchvision.datasets.ImageFolder):
     def __getitem__(self, index):
         img = super(ColorImageNet, self).__getitem__(index)[0]
-        img = (rgb2lab(img) - mean) / std
+        img = (np.float32(color.rgb2lab(img)) - mean) / std
         return img[None, ..., 0], img[..., 1:].transpose(2, 0, 1)
 
 def get_data_loaders(batch_size, dataset, img_size=256):
