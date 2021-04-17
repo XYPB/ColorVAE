e1078a6a8d882e7c8604cb71d29f2fd0d73ece46
XYPB <duyx2000@gmail.com>
2021-04-18 00:05:55 +0800

    high dims latent display

--- main.py
+++ main.py
@@ -43,8 +43,10 @@ def log_img(model, args, wandb, writer):
             save_plt_img(img, title='result')
 
         l = lab[:1, 0].repeat(64, 1, 1, 1)
-        z = torch.meshgrid(torch.linspace(-2, 2, 8), torch.linspace(-2, 2, 8))
-        z = torch.stack(z, -1).flatten(0, 1).to(device)
+        z = model.module.prior.sample(l.size(0)).repeat(64,1)
+        z_ = torch.meshgrid(torch.linspace(-2, 2, 8), torch.linspace(-2, 2, 8))
+        z_ = torch.stack(z, -1).flatten(0, 1).to(device)
+        z[:,:2] = z_
         lab = torch.cat([l, model.module.transform(z, l)], 1)
         img = reconstruct(lab)
         if args.vis_mode == 'tensorboard':
