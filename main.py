15a08809655e5aaf497648c1284d2e95c4a333fd
XYPB <duyx2000@gmail.com>
2021-04-18 13:53:25 +0800

    fix

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def log_img(model, args, wandb, writer):
         l = lab[:1, 0].repeat(64, 1, 1, 1)
         z = model.module.prior.sample(l.size(0)).repeat(64,1)
         z_ = torch.meshgrid(torch.linspace(-2, 2, 8), torch.linspace(-2, 2, 8))
-        z_ = torch.stack(z, -1).flatten(0, 1).to(device)
+        z_ = torch.stack(z_, -1).flatten(0, 1).to(device)
         z[:,:2] = z_
         lab = torch.cat([l, model.module.transform(z, l)], 1)
         img = reconstruct(lab)
